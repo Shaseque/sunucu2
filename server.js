553d38ce58cde442056ff22009665e7d455edce1
shs3131 <tgtygaming60@gmail.com>
2025-05-04 12:03:55 +0000

    🔄 Otomatik commit: 04.05.2025 14:55:55

--- server.js
+++ server.js
@@ -57,7 +57,8 @@ bot.on('kicked', (reason) => {
 bot.on('end', () => {
   console.log('Bot bağlantısı kesildi');
   setTimeout(() => {
-    bot = mineflayer.createBot(botConfig);  // 5 saniye sonra tekrar bağlan
+    bot = mineflayer.createBot(botConfig);
+    bot.loadPlugin(pathfinder);  // 5 saniye sonra tekrar bağlan
   }, 5000);
 });
 
