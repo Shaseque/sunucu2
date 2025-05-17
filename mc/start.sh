e6e1e7e35e005354bed81e03a38bedaaeafb2cb8
shs3131 <tgtygaming60@gmail.com>
2025-05-17 21:36:08 +0000

    🔄 Otomatik commit: 18.05.2025 00:23:46

--- mc/start.sh
+++ mc/start.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 echo "Starting PaperMC Server..."
-java -Xms2G -Xmx4G -jar 31311.jar nogui
+
+# Java optimizasyon flag'leri (Açıklamalar aşağıda)
+java \
+  -server \
+  -Xms4G -Xmx4G \
+  -XX:+UseG1GC \
+  -XX:+ParallelRefProcEnabled \
+  -XX:MaxGCPauseMillis=200 \
+  -XX:+UnlockExperimentalVMOptions \
+  -XX:+DisableExplicitGC \
+  -XX:+AlwaysPreTouch \
+  -XX:G1NewSizePercent=30 \
+  -XX:G1MaxNewSizePercent=40 \
+  -XX:G1HeapRegionSize=8M \
+  -XX:G1ReservePercent=20 \
+  -XX:G1HeapWastePercent=5 \
+  -XX:G1MixedGCCountTarget=4 \
+  -XX:InitiatingHeapOccupancyPercent=15 \
+  -XX:G1MixedGCLiveThresholdPercent=90 \
+  -XX:G1RSetUpdatingPauseTimePercent=5 \
+  -XX:SurvivorRatio=32 \
+  -XX:+PerfDisableSharedMem \
+  -XX:MaxTenuringThreshold=1 \
+  -Dusing.aikars.flags=true \
+  -Dcom.mojang.eula.agree=true \
+  -jar 31311.jar nogui
