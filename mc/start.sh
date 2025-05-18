5d7fb7bc70adbca90e2715017137cbdfe7abd98e
shs3131 <tgtygaming60@gmail.com>
2025-05-18 10:21:29 +0000

    🔄 Otomatik commit: 18.05.2025 13:21:27

--- mc/start.sh
+++ mc/start.sh
@@ -4,7 +4,7 @@ echo "Starting PaperMC Server..."
 # Java optimizasyon flag'leri (Açıklamalar aşağıda)
 java \
   -server \
-  -Xms4G -Xmx4G \
+  -Xms4G -Xmx8G \
   -XX:+UseG1GC \
   -XX:+ParallelRefProcEnabled \
   -XX:MaxGCPauseMillis=200 \
