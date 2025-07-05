95212bbe0354faae12030e22b1e5e663db487c63
Shaseque <124176408+Shaseque@users.noreply.github.com>
2025-07-05 21:08:46 +0000

    🔄 Otomatik commit: 06.07.2025 00:08:45

--- mc/master.sh
+++ mc/master.sh
@@ -2,22 +2,19 @@
 
 mkdir -p logs
 
-# FIFO gerekiyorsa yine kur, belki başka servis kullanıyordur
 rm -f /tmp/mc_input.fifo
 mkfifo /tmp/mc_input.fifo
 
 echo "🚀 Scriptler başlatılıyor..."
 
-# Arka plan servislerini başlat
 bash com.sh > logs/com.log 2>&1 & 
 PID1=$!
 echo "✅ com.sh COMMIT başlatıldı (PID: $PID1)"
 
-bash cloudflared tunnel run --token eyJhIjoiNTdiZjg3Y2NhOWUwYmY4MDBmMDZlMmNlNmVjZjExYjMiLCJ0IjoiMzczZTM3MTctMzM0OC00ZWMwLTk0ZTUtOTU2NGU5NGMzMmJhIiwicyI6IllXWmtNell4TnpVdFptVXlOaTAwTUdJekxUaGlOekl0TnpJek1tUTJPV0ZoTVRKaCJ9  > logs/serveo.log 2>&1 &
+cloudflared tunnel run --token eyJhIjoiNTdiZjg3Y2NhOWUwYmY4MDBmMDZlMmNlNmVjZjExYjMiLCJ0IjoiMzczZTM3MTctMzM0OC00ZWMwLTk0ZTUtOTU2NGU5NGMzMmJhIiwicyI6IllXWmtNell4TnpVdFptVXlOaTAwTUdJekxUaGlOekl0TnpJek1tUTJPV0ZoTVRKaCJ9 > logs/serveo.log 2>&1 & 
 PID2=$!
-echo "✅playit başlatıldı (PID: $PID2)"
+echo "✅ playit başlatıldı (PID: $PID2)"
 
 echo "🌀 Scriptler arka planda çalışıyor. Bu terminali kapatabilirsin."
 
-# Scripti sonlandır
 exit 0
