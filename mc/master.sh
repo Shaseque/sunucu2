8e4ae0c47bb225ef2bff9d8ec166236875e02afe
Shaseque <124176408+Shaseque@users.noreply.github.com>
2025-07-05 21:12:53 +0000

    🔄 Otomatik commit: 06.07.2025 00:08:45

--- mc/master.sh
+++ mc/master.sh
@@ -13,7 +13,7 @@ echo "✅ com.sh COMMIT başlatıldı (PID: $PID1)"
 
 cloudflared tunnel run --token eyJhIjoiNTdiZjg3Y2NhOWUwYmY4MDBmMDZlMmNlNmVjZjExYjMiLCJ0IjoiMzczZTM3MTctMzM0OC00ZWMwLTk0ZTUtOTU2NGU5NGMzMmJhIiwicyI6IllXWmtNell4TnpVdFptVXlOaTAwTUdJekxUaGlOekl0TnpJek1tUTJPV0ZoTVRKaCJ9 > logs/serveo.log 2>&1 & 
 PID2=$!
-echo "✅ playit başlatıldı (PID: $PID2)"
+echo "✅ DOMAIN başlatıldı (PID: $PID2)"
 
 echo "🌀 Scriptler arka planda çalışıyor. Bu terminali kapatabilirsin."
 
