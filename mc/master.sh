4ec1d31c75f94c0756d8e86b273bef65685306d8
Shaseque <124176408+Shaseque@users.noreply.github.com>
2025-07-13 19:11:47 +0000

    🔄 Otomatik commit: 13.07.2025 22:11:43

--- mc/master.sh
+++ mc/master.sh
@@ -15,7 +15,11 @@ playit > logs/playit.log 2>&1 &
 PID2=$!
 echo "✅ playit başlatıldı (PID: $PID2)"
 
-echo "🌀 Scriptler arka planda çalışıyor. Bu terminali kapatabilirsin."
+cloudflared tunnel run --token eyJhIjoiNTdiZjg3Y2NhOWUwYmY4MDBmMDZlMmNlNmVjZjExYjMiLCJ0IjoiMzczZTM3MTctMzM0OC00ZWMwLTk0ZTUtOTU2NGU5NGMzMmJhIiwicyI6Ill6QTRNVEJqT1dJdFpHRXpZaTAwTURsbExXRmxOR1V0TURneE5qVmtNVE00TXpWaiJ9 > logs/cloudflared.log 2>&1 &
+PID3=$!
+echo "✅ cloudflared başlatıldı (PID: $PID3)"
+
+echo "🌀 Tüm scriptler arka planda çalışıyor. Bu terminali kapatabilirsin."
 
 exit 0
 
