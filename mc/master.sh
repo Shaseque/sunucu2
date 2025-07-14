12e8f9ae981b50acc8eaf00d0b1a8e5ef812ccda
Shaseque <124176408+Shaseque@users.noreply.github.com>
2025-07-14 09:09:34 +0000

    🔄 Otomatik commit: 14.07.2025 12:09:33

--- mc/master.sh
+++ mc/master.sh
@@ -15,7 +15,7 @@ playit > logs/playit.log 2>&1 &
 PID2=$!
 echo "✅ playit başlatıldı (PID: $PID2)"
 
-cloudflared tunnel run --token eyJhIjoiNTdiZjg3Y2NhOWUwYmY4MDBmMDZlMmNlNmVjZjExYjMiLCJ0IjoiMzczZTM3MTctMzM0OC00ZWMwLTk0ZTUtOTU2NGU5NGMzMmJhIiwicyI6Ill6QTRNVEJqT1dJdFpHRXpZaTAwTURsbExXRmxOR1V0TURneE5qVmtNVE00TXpWaiJ9 > logs/cloudflared.log 2>&1 &
+cloudflared tunnel run > logs/cloudflared.log 2<&1 &
 PID3=$!
 echo "✅ cloudflared başlatıldı (PID: $PID3)"
 
